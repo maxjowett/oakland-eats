ab34fb04fff66b1ad1ab1eb475ace8aab9752a03
maxjowett <jowett.max@gmail.com>
2019-08-04 22:49:42 -0700

    Add yelp api call on componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Navbar from './components/Navbar/Navbar';
+
+const config = {
+  headers: {'Authorization': `Bearer ${process.env.REACT_APP_YELP_API_KEY}`}
+}
+
+class App extends Component {
+  state = {
+    data: null
+  }
+
+
+  callYelp = () => {
+    axios.get('http://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=oakland', config)
+    .then((response) => console.log(response));
+  }
+
+  componentDidMount() {
+    this.callYelp()
+  }
+
+  render() {
+    console.log(process.env.REACT_APP_YELP_API_KEY)
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
