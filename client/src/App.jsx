9781fa1cd354322b57b76ed7857e6b00d90a085b
maxjowett <jowett.max@gmail.com>
2019-09-09 14:46:54 -0700

    Add initial components

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oakland Eats</h1>
+      <h2>Yeet</h2>
+    </div>
+  );
+}
+
+export default App;
