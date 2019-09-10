f0ac1693baf39a16e8b29a4ae3b4ff7b3c8fcfa8
maxjowett <jowett.max@gmail.com>
2019-09-10 15:49:55 -0700

    Add data + react-google-maps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
+import restaurants from './data.js';
 import Side from './components/Side/Side.jsx';
 import Main from './components/Main/Main.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Oakland Eats</h1>
-      <Side />
-      <Main />
-    </div>
-  );
+class App extends Component {
+  state = {
+    restaurants
+  };
+  render() {
+    console.log(this.state.restaurants);
+    return (
+      <div className="App">
+        <Side />
+        <Main />
+      </div>
+    );
+  }
 }
 
 export default App;
