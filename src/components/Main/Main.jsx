0822acd6aed467f91fa048753993f46e8b0c5dc4
maxjowett <jowett.max@gmail.com>
2019-09-10 17:18:05 -0700

    :lock: Add .env for api keys

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const key = process.env.REACT_APP_GOOGLE_API_KEY;
+
 class Main extends Component {
-  state = {};
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   render() {
     return (
-      <div className="main">
-        <h2>This is the Main</h2>
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}>
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
       </div>
     );
   }
