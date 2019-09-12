1a9332e0eb26bf1667d4e979f7c3518780c8fdb7
maxjowett <jowett.max@gmail.com>
2019-09-12 13:03:37 -0700

    Tidy up side component styling

--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -7,10 +7,18 @@ class Side extends Component {
     return this.props.restaurants.map(restaurant => (
       <div className="restaurant">
         <h2 className="restaurant__title">{restaurant.name}</h2>
-        <h3>{restaurant.display_phone}</h3>
-        <h3>{restaurant.categories[0].title}</h3>
-        <h3>{restaurant.location.display_address[0]}</h3>
-        <h3>{restaurant.location.display_address[1]}</h3>
+        <h3 className="restaurant__category">
+          {restaurant.categories[0].title}
+        </h3>
+        <h3 className="restaurant__phone">
+          {restaurant.display_phone}
+        </h3>
+        <h3 className="restaurant__info">
+          {restaurant.location.display_address[0]}
+        </h3>
+        <h3 className="restaurant__info">
+          {restaurant.location.display_address[1]}
+        </h3>
       </div>
     ));
   };
