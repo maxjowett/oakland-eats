92a621409aad4a563ab0f9cda525281da63708ad
Max Jowett <jowett.max@gmail.com>
2020-10-05 22:05:12 -0700

    Add initial restaurants to plot

---
+++ data/coordinates.js
@@ -0,0 +1,18 @@
+export const places = [
+  {
+    name: 'Taqueria Sinaloa',
+    lat: 37.7981353,
+    long: -122.2383693
+  },
+  {
+    name: 'Noodle Theory',
+    lat: 37.8492649,
+    long: -122.2548642
+  },
+  {
+    name: 'Sliver Pizza',
+    lat: 37.8339371,
+    long: -122.2514668
+  },
+];
+
